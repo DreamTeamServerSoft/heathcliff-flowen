2e5b10fe820a6eb8790f3e5e1b5931088fc679e0
DreamTeam PSO! <dreamteampso@gmail.com>
2021-08-17 18:52:40 -0400

    Added a help command if a user needs some guidance.

--- main.js
+++ main.js
@@ -9,6 +9,13 @@ cli.once("ready", () => {
     console.log("Healthcliff Flowen: Running....")
 });
 
+// '!help' Command.
+cli.on("message", function (message) {
+    if (message.content === "!help") {
+        message.channel.send("Hi there! Assigning a role or using a command is easy. All you need to do is head over to #📝-assign-roles and pick a command or role from the list provided in the channel. Once you find a role you like, you can aquire it by going to #📝-assign-roles and entering a valid command. If you entered the correct command or role, you should get a message from me confirming it.");
+    }
+});
+
 // '!Ask' Command.
 cli.on("message", function (message) {
     if (message.content === "!ask") {
@@ -28,4 +35,4 @@ cli.on("message", function (message) {
 });
 
 // Bot Login Token.
-cli.login("NzEzNzIyODQ2NTQ2NzU1NjI0.XskQMg.QxhNOGij-K5PDehVqzwzM4wHYCg"); // If you are seeing this on Github, this token has expired.np
+cli.login("NzEzNzIyODQ2NTQ2NzU1NjI0.XskQMg.cmpQmSESuznKv9QiYZiqH9kv2eY"); // If you are seeing this on Github, this token has expired.np
