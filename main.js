572b778dd0de52c0e5cfc9ccd92ae8fe9269fc7a
DreamTeam PSO! <dreamteampso@gmail.com>
2021-08-17 19:45:01 -0400

    Updated the readme, added a dice roll command for fun, added dcv1 as an assignable role, added dcv1 as a role you can delete, updated the help command to include how to delete a role/command.

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const { Message } = require("discord.js");
 const Discord = require("Discord.js");
 const cli = new Discord.Client();
 
+// All Server Role (IDs).
+const dcv1 = "700385883395981413"; // PSO Dreamcast Version 1.
+
 // When the client is running, log it in the console.
 cli.once("ready", () => {
     console.clear();
@@ -12,7 +15,7 @@ cli.once("ready", () => {
 // '!help' Command.
 cli.on("message", function (message) {
     if (message.content === "!help") {
-        message.channel.send("Hi there! Assigning a role or using a command is easy. All you need to do is head over to #📝-assign-roles and pick a command or role from the list provided in the channel. Once you find a role you like, you can aquire it by going to #📝-assign-roles and entering a valid command. If you entered the correct command or role, you should get a message from me confirming it.");
+        message.channel.send("Hi there! Assigning a role or using a command is easy. All you need to do is head over to #📝-assign-roles and pick a command or role from the list provided in the channel. Once you find a role you like, you can aquire it by going to #📝-assign-roles and entering a valid command. If you entered the correct command or role, you should get a message from me confirming it. You can also remove roles by going to #📝-assign-roles and typing the command '!remove' followed by a space and the name of the role you'd like to remove. For instance, if you want to remove the role '!role dcv2' you would enter '!remove dcv2'. I hope you found this helpful!");
     }
 });
 
@@ -34,5 +37,31 @@ cli.on("message", function (message) {
     }
 });
 
+// '!roll dice' Command.
+cli.on("message", function (message) {
+    if (message.content === "!roll dice") {
+        // Generate Random Integers Between 1 & 6.
+        const RandInt = Math.floor(Math.random() * 6) + 1;
+
+        // Display Random Int.
+        message.channel.send("- Throws Di On Table -");
+        message.channel.send("The Di Has Landed On Side: (" + RandInt + ")!");
+    }
+});
+
+// '!role dcv1' Command.
+cli.on("message", function (message) {
+    if (message.content == "!role dcv1") {
+        message.member.roles.add(dcv1);
+        message.channel.send("You are now assigned to 'PSO Dreamcast Version 1!'");
+    } else if (message.content == "!remove dcv1") {
+        message.member.roles.remove(dcv1);
+        message.channel.send("You have successfully removed 'PSO Dreamcast Version 1' from your Discord account.");
+    }
+});
+
+
+
+
 // Bot Login Token.
-cli.login("NzEzNzIyODQ2NTQ2NzU1NjI0.XskQMg.cmpQmSESuznKv9QiYZiqH9kv2eY"); // If you are seeing this on Github, this token has expired.np
+cli.login("NzEzNzIyODQ2NTQ2NzU1NjI0.XskQMg.WJzTlf23rtISMvsP3j873RoZ0xE"); // If you are seeing this on Github, this token has expired.np
