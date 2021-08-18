f4b68d3eaa0ceae7d4ac040365630f04aa69cf3a
DreamTeam PSO! <dreamteampso@gmail.com>
2021-08-17 20:26:19 -0400

    Added final PSO game related commands, added a role to let users subscribe to the announcements channel.

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const { Message } = require("discord.js");
 const Discord = require("Discord.js");
 const cli = new Discord.Client();
 
-// All Server Role (IDs).
+// All PSO Game Role (IDs).
 const dcv1 = "700385883395981413"; // PSO Dreamcast Version 1.
 const dcv2 = "700390319027650610"; // PSO Dreamcast Version 2.
 const psogc = "700390658460090389"; // PSO GameCube.
 const psopc = "700386019345956865"; // PSO PC.
 const psobb = "713743313701961749"; // PSO BB.
 const psox = "713744416367444058"; // PSOX.
+const pso2 = "716064125188309024"; // PSO2.
+
+// Other Misc DreamTeam Role (IDs).
+const pingme = "770733104775036929" // Subscribe to 'Announcements'.
 
 // When the client is running, log it in the console.
 cli.once("ready", () => {
@@ -21,6 +25,17 @@ cli.once("ready", () => {
 cli.on("message", function (message) {
     if (message.content === "!help") {
         message.channel.send("Hi there! Assigning a role or using a command is easy. All you need to do is head over to #📝-assign-roles and pick a command or role from the list provided in the channel. Once you find a role you like, you can aquire it by going to #📝-assign-roles and entering a valid command. If you entered the correct command or role, you should get a message from me confirming it. You can also remove roles by going to #📝-assign-roles and typing the command '!remove' followed by a space and the name of the role you'd like to remove. For instance, if you want to remove the role '!role dcv2' you would enter '!remove dcv2'. I hope you found this helpful!");
+    }
+});
+
+// '!role pingme' Command.
+cli.on("message", function (message) {
+    if (message.content == "!role pingme") {
+        message.member.roles.add(pingme);
+        message.channel.send("You are now subscribed to DreamTeam announcements! You will be pinged by the 'Announcements' channel when something new comes to DreamTeam. You will also be notified if a YouTube video is posted on our official YouTube channel.");
+    } else if (message.content == "!remove pingme") {
+        message.member.roles.remove(pingme);
+        message.channel.send("You have unsubscribed from DreamTeam announcements. :(");
     }
 });
 
@@ -120,6 +135,17 @@ cli.on("message", function (message) {
     }
 });
 
+// '!role pso2' Command.
+cli.on("message", function (message) {
+    if (message.content == "!role pso2") {
+        message.member.roles.add(pso2);
+        message.channel.send("You are now assigned to 'PSO 2!'");
+    } else if (message.content == "!remove pso2") {
+        message.member.roles.remove(pso2);
+        message.channel.send("You have successfully removed 'PSO 2' from your Discord account.");
+    }
+});
+
 
 // Bot Login Token.
-cli.login("NzEzNzIyODQ2NTQ2NzU1NjI0.XskQMg.WKcvacXstfnU9Xqh-XiNts5pIlw"); // If you are seeing this on Github, this token has expired.
+cli.login("NzEzNzIyODQ2NTQ2NzU1NjI0.XskQMg.tfqJQaf7Xcsawuy8iG6dv6DxSHE"); // If you are seeing this on Github, this token has expired.
