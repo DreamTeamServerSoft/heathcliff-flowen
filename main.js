4e12c75a048fd4b7b2877070524ac69a781f790b
IAmShawn98 <shawnluther99@gmail.com>
2022-02-08 18:03:38 -0500

    Small code cleanup, new heads or tails coin flip game created!

--- main.js
+++ main.js
@@ -13,132 +13,187 @@ const quake3 = "754023346097750068"; // Quake III Arena.
 const Halo = "902041200159240272"; // Halo
 
 // Other Misc DreamTeam Role (IDs).
-const pingme = "770733104775036929" // Subscribe to 'Announcements'.
+const pingme = "770733104775036929"; // Subscribe to 'Announcements'.
 
 // When the client is running, log it in the console.
 cli.once("ready", () => {
-    console.clear();
-    console.log("Healthcliff Flowen: Running....")
+  console.clear();
+  console.log("Healthcliff Flowen: Running....");
 });
 
 // '!help' Command.
 cli.on("message", function (message) {
-    if (message.content === "!help") {
-        message.channel.send("Hi there! Assigning a role or using a command is easy. All you need to do is head over to #📝-assign-roles and pick a command or role from the list provided in the channel. Once you find a role you like, you can aquire it by going to #📝-assign-roles and entering a valid command. If you entered the correct command or role, you should get a message from me confirming it. You can also remove roles by going to #📝-assign-roles and typing the command '!remove' followed by a space and the name of the role you'd like to remove. For instance, if you want to remove the role '!role dcv2' you would enter '!remove dcv2'. I hope you found this helpful!");
-    }
+  if (message.content === "!help") {
+    message.channel.send(
+      "Hi there! Assigning a role or using a command is easy. All you need to do is head over to #📝-assign-roles and pick a command or role from the list provided in the channel. Once you find a role you like, you can aquire it by going to #📝-assign-roles and entering a valid command. If you entered the correct command or role, you should get a message from me confirming it. You can also remove roles by going to #📝-assign-roles and typing the command '!remove' followed by a space and the name of the role you'd like to remove. For instance, if you want to remove the role '!role dcv2' you would enter '!remove dcv2'. I hope you found this helpful!"
+    );
+  }
 });
 
 // '!role pingme' Command.
 cli.on("message", function (message) {
-    if (message.content == "!role pingme") {
-        message.member.roles.add(pingme);
-        message.channel.send("You are now subscribed to DreamTeam announcements! You will be pinged by the 'Announcements' channel when something new comes to DreamTeam. You will also be notified if a YouTube video is posted on our official YouTube channel.");
-    } else if (message.content == "!remove pingme") {
-        message.member.roles.remove(pingme);
-        message.channel.send("You have unsubscribed from DreamTeam announcements. :(");
-    }
+  if (message.content == "!role pingme") {
+    message.member.roles.add(pingme);
+    message.channel.send(
+      "You are now subscribed to DreamTeam announcements! You will be pinged by the 'Announcements' channel when something new comes to DreamTeam. You will also be notified if a YouTube video is posted on our official YouTube channel."
+    );
+  } else if (message.content == "!remove pingme") {
+    message.member.roles.remove(pingme);
+    message.channel.send(
+      "You have unsubscribed from DreamTeam announcements. :("
+    );
+  }
 });
 
 // '!Ask' Command.
 cli.on("message", function (message) {
-    if (message.content === "!ask") {
-        // Array of Possible Answers.
-        const answers = [
-            "Yes.",
-            "No.",
-            "Sure",
-            "Maybe.",
-            "Definitely.",
-            "Absolutely not.",
-            "I'm busy right now, ask me later.",
-            "I don't get paid enough to answer that question.",
-            "Hey man, I'm trying to finish this poptart, can a guy have a moment?"
-        ];
+  if (message.content === "!ask") {
+    // Array of Possible Answers.
+    const answers = [
+      "Yes.",
+      "No.",
+      "Sure",
+      "Maybe.",
+      "Definitely.",
+      "Absolutely not.",
+      "I'm busy right now, ask me later.",
+      "There is no way I'm answering that.",
+      "I don't get paid enough to answer that question.",
+      "Hey man, I'm trying to finish this poptart, can a guy have a moment?",
+    ];
 
-        // Based On the Above Strings, randomly loop through all possibilities.
-        let answer = answers[Math.floor(Math.random() * answers.length)];
-        // Respond to the user with an answer.
-        message.channel.send(answer);
-    }
+    // Based On the Above Strings, randomly loop through all possibilities.
+    let answer = answers[Math.floor(Math.random() * answers.length)];
+    // Respond to the user with an answer.
+    message.channel.send(answer);
+  }
 });
 
 // '!roll dice' Command.
 cli.on("message", function (message) {
-    if (message.content === "!roll dice") {
-        // Generate Random Integers Between 1 & 6.
-        const RandInt = Math.floor(Math.random() * 6) + 1;
+  if (message.content === "!roll dice") {
+    // Generate Random Integers Between 1 & 6.
+    const RandInt = Math.floor(Math.random() * 6) + 1;
 
-        // Display Random Int.
-        message.channel.send("- Throws Di On Table -");
-        message.channel.send("The Di Has Landed On Side: (" + RandInt + ")!");
-    }
+    // Display Random Int.
+    message.channel.send("- Throws Di On Table -");
+    message.channel.send("The Di Has Landed On Side: (" + RandInt + ")!");
+  }
+});
+
+// '!flipcoin' Command.
+cli.on("message", function (message) {
+  if (message.content === "!flipcoin") {
+    // Coin Flip Ary (Heads or Tails).
+    const aryFlip = [
+      "https://i.postimg.cc/Hkvw4ZqY/heads.png",
+      "https://i.postimg.cc/kMZPzqWK/tails.png",
+    ];
+
+    // Generate Random Integers Between 1 & 2.
+    let flip = aryFlip[Math.floor(Math.random() * aryFlip.length)];
+
+    // Heathcliff's Epic Monolouge!
+    setTimeout(() => {
+      message.channel.send(
+        "Alright suckers, it's time to play 'Coin Tawce!™'. If the coin lands on MY MUG (heads), you LOOSE!"
+      );
+    }, 1000);
+    setTimeout(() => {
+      message.channel.send(
+        "On the other side, if you reveal a GAE BOLG (tails), sadly, you win."
+      );
+    }, 4000);
+    setTimeout(() => {
+      message.channel.send("Think you have what it takes? Let's do this.");
+    }, 5400);
+    setTimeout(() => {
+      message.channel.send("- Flips Coin -");
+    }, 6800);
+    setTimeout(() => {
+      // Display Head or Tail of 'Coin'.
+      message.channel.send({ files: [flip] });
+    }, 8200);
+  }
 });
 
 // '!role dcv1' Command.
 cli.on("message", function (message) {
-    if (message.content == "!role dcv1") {
-        message.member.roles.add(dcv1);
-        message.channel.send("You are now assigned to 'PSO Dreamcast Version 1!'");
-    } else if (message.content == "!remove dcv1") {
-        message.member.roles.remove(dcv1);
-        message.channel.send("You have successfully removed 'PSO Dreamcast Version 1' from your Discord account.");
-    }
+  if (message.content == "!role dcv1") {
+    message.member.roles.add(dcv1);
+    message.channel.send("You are now assigned to 'PSO Dreamcast Version 1!'");
+  } else if (message.content == "!remove dcv1") {
+    message.member.roles.remove(dcv1);
+    message.channel.send(
+      "You have successfully removed 'PSO Dreamcast Version 1' from your Discord account."
+    );
+  }
 });
 
 // '!role dcv2' Command.
 cli.on("message", function (message) {
-    if (message.content == "!role dcv2") {
-        message.member.roles.add(dcv2);
-        message.channel.send("You are now assigned to 'PSO Dreamcast Version 2!'");
-    } else if (message.content == "!remove dcv2") {
-        message.member.roles.remove(dcv2);
-        message.channel.send("You have successfully removed 'PSO Dreamcast Version 2' from your Discord account.");
-    }
+  if (message.content == "!role dcv2") {
+    message.member.roles.add(dcv2);
+    message.channel.send("You are now assigned to 'PSO Dreamcast Version 2!'");
+  } else if (message.content == "!remove dcv2") {
+    message.member.roles.remove(dcv2);
+    message.channel.send(
+      "You have successfully removed 'PSO Dreamcast Version 2' from your Discord account."
+    );
+  }
 });
 
 // '!role psogc' Command.
 cli.on("message", function (message) {
-    if (message.content == "!role psogc") {
-        message.member.roles.add(psogc);
-        message.channel.send("You are now assigned to 'PSO GameCube!'");
-    } else if (message.content == "!remove psogc") {
-        message.member.roles.remove(psogc);
-        message.channel.send("You have successfully removed 'PSO GameCube' from your Discord account.");
-    }
+  if (message.content == "!role psogc") {
+    message.member.roles.add(psogc);
+    message.channel.send("You are now assigned to 'PSO GameCube!'");
+  } else if (message.content == "!remove psogc") {
+    message.member.roles.remove(psogc);
+    message.channel.send(
+      "You have successfully removed 'PSO GameCube' from your Discord account."
+    );
+  }
 });
 
 // '!role psopc' Command.
 cli.on("message", function (message) {
-    if (message.content == "!role psopc") {
-        message.member.roles.add(psopc);
-        message.channel.send("You are now assigned to 'PSO PC!'");
-    } else if (message.content == "!remove psopc") {
-        message.member.roles.remove(psopc);
-        message.channel.send("You have successfully removed 'PSO PC' from your Discord account.");
-    }
+  if (message.content == "!role psopc") {
+    message.member.roles.add(psopc);
+    message.channel.send("You are now assigned to 'PSO PC!'");
+  } else if (message.content == "!remove psopc") {
+    message.member.roles.remove(psopc);
+    message.channel.send(
+      "You have successfully removed 'PSO PC' from your Discord account."
+    );
+  }
 });
 
 // '!role quake3' Command.
 cli.on("message", function (message) {
-    if (message.content == "!role quake3") {
-        message.member.roles.add(quake3);
-        message.channel.send("You are now assigned to 'Quake III Arena!'");
-    } else if (message.content == "!remove quake3") {
-        message.member.roles.remove(quake3);
-        message.channel.send("You have successfully removed 'Quake III Arena' from your Discord account.");
-    }
+  if (message.content == "!role quake3") {
+    message.member.roles.add(quake3);
+    message.channel.send("You are now assigned to 'Quake III Arena!'");
+  } else if (message.content == "!remove quake3") {
+    message.member.roles.remove(quake3);
+    message.channel.send(
+      "You have successfully removed 'Quake III Arena' from your Discord account."
+    );
+  }
 });
 
 // '!role halo' Command.
 cli.on("message", function (message) {
-    if (message.content == "!role halo") {
-        message.member.roles.add(Halo);
-        message.channel.send("You are now assigned to 'Halo!'");
-    } else if (message.content == "!remove halo") {
-        message.member.roles.remove(Halo);
-        message.channel.send("You have successfully removed 'Halo' from your Discord account.");
-    }
+  if (message.content == "!role halo") {
+    message.member.roles.add(Halo);
+    message.channel.send("You are now assigned to 'Halo!'");
+  } else if (message.content == "!remove halo") {
+    message.member.roles.remove(Halo);
+    message.channel.send(
+      "You have successfully removed 'Halo' from your Discord account."
+    );
+  }
 });
 
 // Bot Login Token.
-cli.login("NzEzNzIyODQ2NTQ2NzU1NjI0.XskQMg.Am1rojhcIW0wQQpTQoktGC9llhk"); // If you are seeing this on Github, this token has expired.
+cli.login("NzEzNzIyODQ2NTQ2NzU1NjI0.XskQMg.VolZ6wc5O4odk7BZPsdbRln3VF8"); // If you are seeing this on Github, this token has expired.
